3441c67e10b07974fc81b26b44f8dc873d2a29df
Animesh Mohan <animeshmohan@gmail.com>
2020-08-07 17:03:09 -0700

    Create Data insertion function

--- Risk_Analysis/api/views.py
+++ Risk_Analysis/api/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from .models import User
 from .serializers import UserSerializer, UserDataSerializer
 from rest_framework import viewsets
 
 class UserViewSet(viewsets.ModelViewSet):
-    e = User.objects.create(name='Animesh Mohan', risk_score=100, category_avg=[{ 'category_name': "toxic", 'score': 5 },{ 'category_name': "severe_toxic", 'score': 3 },{ 'category_name': "obscene", 'score': 2 },{ 'category_name': "threat", 'score': 6 },{ 'category_name': "insult", 'score': 7 },{ 'category_name': "identity_hate", 'score': 6 },],risk_run=[{'year':2021,'score':23},{'year':1990,'score':50},{'year':2000,'score':50}])
     queryset = User.objects.values('id','name')
     serializer_class = UserSerializer
     http_method_names = ['get']
@@ -14,5 +13,8 @@ class UserDataViewSet(viewsets.ModelViewSet):
     serializer_class = UserDataSerializer
     http_method_names = ['get']
 
+def insert_data(request):
+    User.objects.create(name='Karishma Joseph', risk_score=23, category_avg=[{ 'category_name': "toxic", 'score': 100 },{ 'category_name': "severe_toxic", 'score': 100 },{ 'category_name': "obscene", 'score': 2 },{ 'category_name': "threat", 'score': 6 },{ 'category_name': "insult", 'score': 7 },{ 'category_name': "identity_hate", 'score': 6 },],risk_run=[{'year':2021,'score':23},{'year':1990,'score':50},{'year':2000,'score':50}])
+    return HttpResponse(content=bytes("data,entered",encoding="UTF-8"))
 
 # Create your views here.
