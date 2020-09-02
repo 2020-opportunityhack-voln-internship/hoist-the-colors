a853a5d62fc212169d42fdc35fce651220e10ccc
Karishma-Joseph <karishmajoseph70@gmail.com>
2020-09-01 17:23:48 -0700

    Preprocess fetched data

---
+++ Risk_Analysis/scoring/preprocess.py
@@ -0,0 +1,57 @@
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+def preprocess(text):
+    print('in preprocess')
+    # convert to lower case
+    text = text.lower()
+
+    # remove links and numbers
+    text = re.sub(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)",
+                  "", text)
+    text = re.sub(r"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}", "", text)
+
+    # spelling check (Takes a really long time)
+    # text = str(TextBlob(text).correct())
+
+    # abbreviations
+    text = re.sub(r"what's", "what is ", text)
+    text = re.sub(r"\'s", " ", text)
+    text = re.sub(r"\'ve", " have ", text)
+    text = re.sub(r"can't", "cannot ", text)
+    text = re.sub(r"n't", " not ", text)
+    text = re.sub(r"i'm", "i am ", text)
+    text = re.sub(r"\'re", " are ", text)
+    text = re.sub(r"\'d", " would ", text)
+    text = re.sub(r"\'ll", " will ", text)
+    text = re.sub(r",", " ", text)
+    text = re.sub(r"\.", " ", text)
+    text = re.sub(r"!", " ! ", text)
+    text = re.sub(r"\/", " ", text)
+    text = re.sub(r"\?", " ? ", text)
+    text = re.sub(r"\!", " ! ", text)
+    text = re.sub(r"\"", " ", text)
+    text = re.sub(r"\^", " ^ ", text)
+    text = re.sub(r"\+", " + ", text)
+    text = re.sub(r"\-", " - ", text)
+    text = re.sub(r"\=", " = ", text)
+    text = re.sub(r"'", " ", text)
+    text = re.sub(r":", " : ", text)
+    text = re.sub(r" e g ", " eg ", text)
+    text = re.sub(r" b g ", " bg ", text)
+    text = re.sub(r"\0s", "0", text)
+    text = re.sub(r" 9 11 ", "911", text)
+    text = re.sub(r"e - mail", "email", text)
+    text = re.sub(r"j k", "jk", text)
+
+    # remove special characters
+    text = re.sub(r'[^?!.,:a-z\d ]', '', text, flags=re.IGNORECASE)
+    print('before stop words')
+    # stop word removal
+    STOPWORDS = set(stopwords.words('english'))
+    print('seet of stop words')
+    text = " ".join([word for word in str(text).split() if word not in STOPWORDS])
+
+    return text
--- Risk_Analysis/scoring/views.py
+++ Risk_Analysis/scoring/views.py
@@ -4,6 +4,7 @@ from authentication.views.encrypt_access import *
 import json
 from tweepy import OAuthHandler, API, Cursor
 from requests_oauthlib import OAuth2Session
+from scoring.preprocess import preprocess
 
 
 # Create your views here.
@@ -17,12 +18,17 @@ def fetch_access_tokens():
         db = conn.database
         # Created or Switched to collection names: tokens
         collection = db.tokens
+        print('connected to collection')
         record = collection.find_one()
+        print('fetched record')
         collection.delete_one(record)
+        print('record deleted')
         access_tokens = json.loads(decrypt(record['access_token'], key).decode('utf-8'))
-        if access_tokens['twitter_access_token'] and access_tokens['twitter_access_token_secret']:
+        if 'twitter_access_token' in access_tokens and access_tokens['twitter_access_token'] and access_tokens['twitter_access_token_secret']:
+            print('here')
             fetch_twitter_data(access_tokens['twitter_access_token'], access_tokens['twitter_access_token_secret'])
-        if access_tokens['facebook_access_token']:
+        print('After twitter')
+        if 'facebook_access_token' in access_tokens and access_tokens['facebook_access_token']:
             fetch_facebook_data(access_tokens['facebook_access_token'])
         conn.close()
     except:
@@ -38,14 +44,15 @@ def fetch_twitter_data(access_token, access_token_secret):
     consumer_secret = credentials['twitter_consumer_secret']
 
     callback = 'http://127.0.0.1:8000/authentication/twitter/callback'
-
+    print('in fetch twitter data')
     oauth = OAuthHandler(consumer_key, consumer_secret, callback)
     oauth.set_access_token(access_token, access_token_secret)
     api = API(oauth)
     statuses = []
     for status in Cursor(api.user_timeline).items():
-        statuses.append(status.text)
-    preprocess(statuses)
+        statuses.append(preprocess(status.text))
+    print(statuses)
+    # preprocess(statuses)
 
 
 def fetch_facebook_data(access_token):
@@ -64,10 +71,6 @@ def fetch_facebook_data(access_token):
         if i > 3:
             break
         if 'message' in x:
-            posts.append(x['message'])
-    preprocess(posts)
-    print('facebook data')
-
-
-def preprocess(statuses):
-    print("inside scoring:", statuses)
+            posts.append(preprocess(x['message']))
+    # preprocess(posts)
+    print(posts)
